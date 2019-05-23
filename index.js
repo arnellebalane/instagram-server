da1a3eba5eba8ef3cfade04c26494d83fb39b808
Arnelle Balane <arnellebalane@gmail.com>
2019-05-24 03:38:15 +0800

    Define apollo server, with resolver for posts query

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-console.log('Hello world!');
+const {
+    ApolloServer,
+    gql
+} = require('apollo-server');
+const posts = require('./data/posts.json');
+
+const typeDefs = gql`
+    type Query {
+        posts: [Post]
+    }
+
+    type Post {
+        id: ID!
+        caption: String
+        like_count: Int
+        comments_count: Int
+        media_url: String
+        permalink: String
+    }
+`;
+
+const resolvers = {
+    Query: {
+        posts(parent, args) {
+            return posts;
+        }
+    }
+};
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    playground: true,
+    introspection: true
+});
+
+server.listen().then(({url}) => {
+    console.log(`Server listening on ${url}`);
+});
