f79169f5785b7a97eabba48cf2d9da86a881bb7e
Arnelle Balane <arnellebalane@gmail.com>
2019-05-24 22:18:56 +0800

    Define all remaining queries and resolvers

--- index.js
+++ index.js
@@ -3,10 +3,14 @@ const {
     gql
 } = require('apollo-server');
 const posts = require('./data/posts.json');
+const users = require('./data/users.json');
 
 const typeDefs = gql`
     type Query {
         posts: [Post]
+        post(id: ID!): Post
+        users: [User]
+        user(id: ID!): User
     }
 
     type Post {
@@ -17,12 +21,27 @@ const typeDefs = gql`
         media_url: String
         permalink: String
     }
+
+    type User {
+        id: ID!
+        name: String
+        handle: String
+    }
 `;
 
 const resolvers = {
     Query: {
         posts(parent, args) {
             return posts;
+        },
+        post(parent, args) {
+            return posts.find(post => post.id === args.id);
+        },
+        users(parent, args) {
+            return users;
+        },
+        user(parent, args) {
+            return users.find(user => user.id === args.id);
         }
     }
 };
